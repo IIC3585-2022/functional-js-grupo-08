31acf5e59224cf5dcef2551cf77210928d4086e8
VicenteVicente <vecalisto@uc.cl>
2022-03-16 20:23:36 -0300

    changed wrong file

--- T1.js
+++ T1.js
@@ -1,60 +1,78 @@
+// Imports
 const readlineSync = require('readline-sync');
 
-const initPlayer = (name) => ({ name, score: 501 });
-
-const initGame = (name1, name2) => {
-	console.log(`Juego inicializado con jugadores ${name1} y ${name2}`);
-	return [initPlayer(name1), initPlayer(name2)];
+// TEXTS
+const TEXTS = {
+	init: ({ username1, username2 }) =>
+		`Juego inicializado con jugadores ${username1} y ${username2}`,
+	askThrow: ({ username, i }) =>
+		[
+			`Ingrese el lanzamiento N°${i} de ${username}`,
+			'1. Double Bull (DB)',
+			'2. Single Bull (SB)',
+			'3. Null',
+			'4. Otro\n',
+		].join('\n'),
+	multBase: () => 'Ingrese el puntaje base (1-20)\n',
+	multMultiplier: () => 'Ingrese el multiplicador (1-3)\n',
+	badChoice: () => 'Opción inválida, por favor inténtelo nuevamente\n',
+	currScore: ({ username, score }) => `¡${username} queda con ${score} puntos!`,
+	win: ({ username }) => `¡¡¡EL JUGADOR ${username} HA GANADO!!!`,
 };
 
-const multiplierThrow = () => {
-	const base = readlineSync.question(`Ingrese el puntaje base (1-20)\n`);
-	const multiplier = readlineSync.question(`Ingrese el multiplicador (1-3)\n`);
-	return parseInt(base) * parseInt(multiplier);
+const Player = (name) => {
+	const username = name;
+	let score = 501;
+	let roundScore = 0;
+
+	const multiplierThrow = () => {
+		const base = parseInt(readlineSync.question(TEXTS.multBase()));
+		const multiplier = parseInt(readlineSync.question(TEXTS.multMultiplier()));
+		return base * multiplier;
+	};
+	const makeThrow = (i) => {
+		const choice = parseInt(
+			readlineSync.question(TEXTS.askThrow({ username, i }))
+		);
+		if (choice === 1) return 50;
+		if (choice === 2) return 25;
+		if (choice === 3) return 0;
+		if (choice === 4) return multiplierThrow();
+	};
+
+	return {
+		updateScore: () => {
+			score = Math.abs(score - roundScore);
+			roundScore = 0;
+		},
+		checkScore: () => {
+			console.log(TEXTS.currScore({ username, score }));
+			if (score === 0) {
+				console.log(TEXTS.win({ username }));
+				process.exit(0);
+			}
+		},
+		makeThrows: (nThrows) => {
+			for (let i = 0; i < nThrows; i++) roundScore += makeThrow(i + 1);
+		},
+	};
 };
 
-const makeThrow = (player, i) => {
-	const choice = readlineSync.question(
-		`
-Ingrese el lanzamiento N°${i} de ${player.name}
-1. Double Bull (DB)
-2. Single Bull (SB)
-3. Null
-4. Otro\n`
-	);
-	if (choice === '1') return 50;
-	if (choice === '2') return 25;
-	if (choice === '3') return 0;
-	if (choice === '4') return multiplierThrow();
-};
-
-const makeThrows = (player) => {
-	let scores = 0;
-	for (let i = 1; i < 4; i++) scores += makeThrow(player, i);
-	return scores;
-};
-
-const computeScore = (player) => {
-	const newScore = Math.abs(player.score - makeThrows(player));
-	console.log(`¡${player.name} queda con ${newScore} puntos!`);
-	return newScore;
-};
-
-const checkScore = (player) => {
-	if (player.score !== 0) return false;
-	console.log(`¡¡¡${player.name} HA GANADO!!!`);
-	return true;
-};
+// Functions
+const initGame = (...names) => names.map((name) => Player(name));
 
 const playGame = (name1, name2) => {
-	const [player1, player2] = initGame(name1, name2);
+	const players = initGame(name1, name2);
+	console.log(TEXTS.init({ username1: name1, username2: name2 }));
 
 	while (true) {
-		player1.score = computeScore(player1);
-		if (checkScore(player1)) break;
-		player2.score = computeScore(player2);
-		if (checkScore(player2)) break;
+		players.forEach((player) => {
+			player.makeThrows(3);
+			player.updateScore();
+			player.checkScore();
+		});
 	}
 };
 
+// Instance
 playGame('Jaime', 'Ema');
